d89d8eb1acb2224f852f9fb8c7edcf3fae920fa4
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-09-28 00:55:50 +0300

    ruth

--- 106/CRM/API/calls/callsModel/callModel.ts
+++ 106/CRM/API/calls/callsModel/callModel.ts
@@ -1,19 +1,47 @@
 import mongoose, { model } from "mongoose";
 import { Department } from "../../enums/departments";
 import { Status } from "../../enums/status";
+import { User } from "../../users/userModel";
 const { Schema } = mongoose;
 
-export const CallSchema = new Schema({
-  operatorEmail: String,
-  name: String,
-  phoneNum: String,
+export class Call {
+  id: string;
+  constructor(
+    public fullName: string,
+    public phone: string,
+    public date: string,
+    public dept: Department,
+    public status: string,
+  ) {
+    this.id = Math.random().toString(36).substr(2, 9);
+  }
+
+  changeStatus(newStatus: Status) {
+    this.status = newStatus;
+  }
+}
+
+export const userCalls: [] = [];
+
+const CallSchema = new Schema({
+  fullName: { type: String, required: true },
+  phone: { type: String, required: true },
+  date: { type: String, required: true },
+  dept: {
+    type: String,
+    enum: Object.values(Status),
+    default: Department.Operations,
+  },
   callInfo: String,
-  department: Department,
-  status: Status,
+  status: { type: String, enum: Object.values(Status), default: Status.Open },
+  user: { type: Schema.Types.ObjectId, ref: "users", required: true }, // Reference to the user
 });
 
-const CallModel = model("calls", CallSchema);
-export default CallModel;
+const calls = model("calls", CallSchema);
+
+export const _calls: Call[] = [];
+export default calls;
+// -------------------------------------------------------------------------------------
 
 // enum DepartmentType {
 //   Department1 = "תכנון עירוני",
--- 106/CRM/API/calls/callsModel/dist/callModel.js
+++ 106/CRM/API/calls/callsModel/dist/callModel.js
@@ -1,20 +1,43 @@
 "use strict";
 exports.__esModule = true;
-exports.CallSchema = void 0;
+exports._calls = exports.userCalls = exports.Call = void 0;
 var mongoose_1 = require("mongoose");
 var departments_1 = require("../../enums/departments");
 var status_1 = require("../../enums/status");
 var Schema = mongoose_1["default"].Schema;
-exports.CallSchema = new Schema({
-    operatorEmail: String,
-    name: String,
-    phoneNum: String,
+var Call = /** @class */ (function () {
+    function Call(fullName, phone, date, dept, status) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.date = date;
+        this.dept = dept;
+        this.status = status;
+        this.id = Math.random().toString(36).substr(2, 9);
+    }
+    Call.prototype.changeStatus = function (newStatus) {
+        this.status = newStatus;
+    };
+    return Call;
+}());
+exports.Call = Call;
+exports.userCalls = [];
+var CallSchema = new Schema({
+    fullName: { type: String, required: true },
+    phone: { type: String, required: true },
+    date: { type: String, required: true },
+    dept: {
+        type: String,
+        "enum": Object.values(status_1.Status),
+        "default": departments_1.Department.Operations
+    },
     callInfo: String,
-    department: departments_1.Department,
-    status: status_1.Status
+    status: { type: String, "enum": Object.values(status_1.Status), "default": status_1.Status.Open },
+    user: { type: Schema.Types.ObjectId, ref: "users", required: true }
 });
-var CallModel = mongoose_1.model("calls", exports.CallSchema);
-exports["default"] = CallModel;
+var calls = mongoose_1.model("calls", CallSchema);
+exports._calls = [];
+exports["default"] = calls;
+// -------------------------------------------------------------------------------------
 // enum DepartmentType {
 //   Department1 = "תכנון עירוני",
 //   Department2 = "איכות הסביבה",
