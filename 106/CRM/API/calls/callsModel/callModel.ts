492cc87171f68891f24167d5cbaf07811ddc6192
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-09-28 00:40:18 +0300

    ruth

--- 106/CRM/API/calls/callsModel/callModel.ts
+++ 106/CRM/API/calls/callsModel/callModel.ts
@@ -1,25 +1,26 @@
 import mongoose, { model } from "mongoose";
 import { Department } from "../../enums/departments";
+import { Status } from "../../enums/status";
 const { Schema } = mongoose;
 
-enum DepartmentType {
-  Department1 = "תכנון עירוני",
-  Department2 = "איכות הסביבה",
-  Department3 = "הנדסת תנועה",
-  Department4 = "בעלי חיים",
-  Department5 = "מפגעי תברואה",
-  Department6 = "ביטחון",
-  Department7 = "תחזוקת רחובות",
-  Department8 = "פרויקטים מיוחדים",
-  Department9 = "תחבורה ציבורית",
-  Department10 = "תקלות תחזוקה ",
-}
+// enum DepartmentType {
+//   Department1 = "תכנון עירוני",
+//   Department2 = "איכות הסביבה",
+//   Department3 = "הנדסת תנועה",
+//   Department4 = "בעלי חיים",
+//   Department5 = "מפגעי תברואה",
+//   Department6 = "ביטחון",
+//   Department7 = "תחזוקת רחובות",
+//   Department8 = "פרויקטים מיוחדים",
+//   Department9 = "תחבורה ציבורית",
+//   Department10 = "תקלות תחזוקה ",
+// }
 
-enum Status {
-  done = "done",
-  inProcess = "inProcess",
-  received = "received",
-}
+// enum Status {
+//   done = "done",
+//   inProcess = "inProcess",
+//   received = "received",
+// }
 
 export const CallSchema = new Schema({
   operatorEmail: String,
--- 106/CRM/API/calls/callsModel/dist/callModel.js
+++ 106/CRM/API/calls/callsModel/dist/callModel.js
@@ -2,33 +2,32 @@
 exports.__esModule = true;
 exports.CallSchema = void 0;
 var mongoose_1 = require("mongoose");
+var status_1 = require("../../enums/status");
 var Schema = mongoose_1["default"].Schema;
-var DepartmentType;
-(function (DepartmentType) {
-    DepartmentType["Department1"] = "\u05EA\u05DB\u05E0\u05D5\u05DF \u05E2\u05D9\u05E8\u05D5\u05E0\u05D9";
-    DepartmentType["Department2"] = "\u05D0\u05D9\u05DB\u05D5\u05EA \u05D4\u05E1\u05D1\u05D9\u05D1\u05D4";
-    DepartmentType["Department3"] = "\u05D4\u05E0\u05D3\u05E1\u05EA \u05EA\u05E0\u05D5\u05E2\u05D4";
-    DepartmentType["Department4"] = "\u05D1\u05E2\u05DC\u05D9 \u05D7\u05D9\u05D9\u05DD";
-    DepartmentType["Department5"] = "\u05DE\u05E4\u05D2\u05E2\u05D9 \u05EA\u05D1\u05E8\u05D5\u05D0\u05D4";
-    DepartmentType["Department6"] = "\u05D1\u05D9\u05D8\u05D7\u05D5\u05DF";
-    DepartmentType["Department7"] = "\u05EA\u05D7\u05D6\u05D5\u05E7\u05EA \u05E8\u05D7\u05D5\u05D1\u05D5\u05EA";
-    DepartmentType["Department8"] = "\u05E4\u05E8\u05D5\u05D9\u05E7\u05D8\u05D9\u05DD \u05DE\u05D9\u05D5\u05D7\u05D3\u05D9\u05DD";
-    DepartmentType["Department9"] = "\u05EA\u05D7\u05D1\u05D5\u05E8\u05D4 \u05E6\u05D9\u05D1\u05D5\u05E8\u05D9\u05EA";
-    DepartmentType["Department10"] = "\u05EA\u05E7\u05DC\u05D5\u05EA \u05EA\u05D7\u05D6\u05D5\u05E7\u05D4 ";
-})(DepartmentType || (DepartmentType = {}));
-var Status;
-(function (Status) {
-    Status["done"] = "done";
-    Status["inProcess"] = "inProcess";
-    Status["received"] = "received";
-})(Status || (Status = {}));
+// enum DepartmentType {
+//   Department1 = "תכנון עירוני",
+//   Department2 = "איכות הסביבה",
+//   Department3 = "הנדסת תנועה",
+//   Department4 = "בעלי חיים",
+//   Department5 = "מפגעי תברואה",
+//   Department6 = "ביטחון",
+//   Department7 = "תחזוקת רחובות",
+//   Department8 = "פרויקטים מיוחדים",
+//   Department9 = "תחבורה ציבורית",
+//   Department10 = "תקלות תחזוקה ",
+// }
+// enum Status {
+//   done = "done",
+//   inProcess = "inProcess",
+//   received = "received",
+// }
 exports.CallSchema = new Schema({
     operatorEmail: String,
     name: String,
     phoneNum: String,
     callInfo: String,
     department: DepartmentType,
-    status: Status
+    status: status_1.Status
 });
 var CallModel = mongoose_1.model("calls", exports.CallSchema);
 exports["default"] = CallModel;
