72568bba1526360d7de7c16cc718c22bec3098ef
Lee_D <leegreat21@gmail.com>
2023-09-27 16:42:10 +0300

    model-refinement

---
+++ 106/CRM/API/enums/dist/status.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Status = void 0;
+var Status;
+(function (Status) {
+    Status["Open"] = "Open";
+    Status["InProcess"] = "InProcess";
+    Status["Closed"] = "Closed";
+})(Status = exports.Status || (exports.Status = {}));
