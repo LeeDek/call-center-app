8285a6172c77a209a484fb7c04606919192d3259
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-09-28 00:35:55 +0300

    ruth

---
+++ 106/CRM/API/calls/callsCont/callsContFilters.ts
@@ -0,0 +1,3 @@
+export async function getDoneCalls(req, res){
+    
+}
--- 106/CRM/API/calls/callsRoutes/callsRoutes.ts
+++ 106/CRM/API/calls/callsRoutes/callsRoutes.ts
@@ -1,13 +1,22 @@
-import express from 'express';
-import { addCall, getCalls, deleteCall, getUserCalls} from '../callsCont';
+import express from "express";
+import {
+  addCall,
+  getCalls,
+  deleteCall,
+  getUserCalls,
+} from "../callsCont/callsCont";
+import { getDoneCalls } from "../callsCont/callsContFilters";
 
 const router = express.Router();
 
 router
-.post('/add-call', addCall) // Use POST for adding relatives
-.get('/get-calls', getCalls)
-.delete('/delete-call', deleteCall)
-// .patch('/update-call', updateCall)
-.get('/get-user-calls', getUserCalls)
+  .post("/add-call", addCall) // Use POST for adding relatives
+  .get("/get-calls", getCalls)
+  .delete("/delete-call", deleteCall)
+  // .patch('/update-call', updateCall)
+  .get("/get-user-calls", getUserCalls)
+
+  //filter calls
+  .get("/get-done-calls", getDoneCalls);
 
 export default router;
