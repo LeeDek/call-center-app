1d96f0110b235527e292d76e4a7a72a1fe9a8806
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-09-28 00:44:17 +0300

    ruth

--- 106/CRM/API/calls/callsCont/callsContFilters.ts
+++ 106/CRM/API/calls/callsCont/callsContFilters.ts
@@ -1,3 +1,9 @@
+import { users, UserModel } from "../../users/userModel";
+import { calls, Call, userCalls } from "../callsModel/callModel";
+import { Department } from "../enums/departments";
+import { Status } from "../enums/status";
+
+
 export async function getDoneCalls(req, res){
-    
+
 }
