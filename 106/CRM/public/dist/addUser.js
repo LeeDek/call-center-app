0638ed49a01853a261cf4e4390f8bbe4bcba3862
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-09-29 10:46:30 +0300

    ruht

--- 106/CRM/public/dist/addUser.js
+++ 106/CRM/public/dist/addUser.js
@@ -34,22 +34,21 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-function handleLogin(ev) {
+function handleRegister(ev) {
     return __awaiter(this, void 0, void 0, function () {
-        var user, response, _a, error, role, error_1;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
+        var user, response, error, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
                 case 0:
-                    _b.trys.push([0, 3, , 4]);
+                    _a.trys.push([0, 3, , 4]);
                     ev.preventDefault(); // stop form from submitting
                     user = {
-                        password: ev.target.password.value,
-                        email: ev.target.email.value
+                        userName: ev.target.userName.value,
+                        email: ev.target.email.value,
+                        role: ev.target.role.value
                     };
-                    if (!user.email || !user.password) {
-                        throw new Error("Please complete all fields");
-                    }
-                    return [4 /*yield*/, fetch("/API/users/login", {
+                    return [4 /*yield*/, fetch("/API/users/add-user", {
+                            // send data to server
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
@@ -57,29 +56,15 @@ function handleLogin(ev) {
                             body: JSON.stringify(user)
                         })];
                 case 1:
-                    response = _b.sent();
+                    response = _a.sent();
                     return [4 /*yield*/, response.json()];
                 case 2:
-                    _a = _b.sent(), error = _a.error, role = _a.role;
-                    console.log(error);
-                    if (!role)
-                        throw new Error("cannot detect user role");
+                    error = (_a.sent()).error;
                     if (error)
                         throw new Error(error);
-                    switch (role) {
-                        case 'admin':
-                            window.location.href = "/admin.html";
-                            break;
-                        case 'departmentMng':
-                            window.location.href = "/manager.html";
-                            break;
-                        case 'user':
-                            window.location.href = "/main.html";
-                            break;
-                    }
                     return [3 /*break*/, 4];
                 case 3:
-                    error_1 = _b.sent();
+                    error_1 = _a.sent();
                     console.error(error_1);
                     return [3 /*break*/, 4];
                 case 4: return [2 /*return*/];
